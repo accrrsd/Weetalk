d449e841c4f1af598399db2871ed3c7c42607480
Daniil <kashel369@bk.ru>
2023-01-08 18:57:41 +0300

    fix base routing

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,50 +1,39 @@
-import style from './App.module.css';
-import { useEffect, useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import style from './App.module.css'
+import { useEffect, useState } from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
 
-import { ProtectedRoutes } from '../protected-routes/protected-routes';
+import { ProtectedRoutes } from '../protected-routes/protected-routes'
 
-import Login from '../../pages/login';
-import { RouteMenu } from '../route-menu/route-menu';
-import Profile from '../../pages/Profile';
-import Main from '../../pages/main';
-import Welcome from '../../pages/welcome';
+import Login from '../../pages/login'
+import { RouteMenu } from '../route-menu/route-menu'
+import Profile from '../../pages/Profile'
+import Main from '../../pages/main'
+import Welcome from '../../pages/welcome'
 
 export default function App() {
-  const [userAuthorized, setUserAuthorized] = useState(false);
+  const [userAuthorized, setUserAuthorized] = useState(false)
+  const location = useLocation()
 
   useEffect(() => {
     // Получаем из localStorage статус авторизации
-    setUserAuthorized(!!localStorage.getItem('userData'));
-  }, []);
+    setUserAuthorized(!!localStorage.getItem('userData'))
+  }, [])
 
   return (
     <div className={style.page}>
       <Routes>
-        <Route
-          element={
-            <ProtectedRoutes needAuthorized={true} auth={userAuthorized} />
-          }
-        >
-          {/* Изначальный экран - экран гостей */}
+        <Route element={<ProtectedRoutes needAuthorized={false} auth={userAuthorized} />}>
+          <Route path="/login" element={<Login authorizedFunc={setUserAuthorized} />} />
+        </Route>
+        <Route element={<ProtectedRoutes needAuthorized={true} auth={userAuthorized} />}>
           <Route path="/" element={<Welcome />} />
           <Route path="/guests" element={<Main />} />
           <Route path="/favorite" />
           <Route path="/recommendations" />
           <Route path="/profile" element={<Profile />} />
         </Route>
-        <Route
-          element={
-            <ProtectedRoutes needAuthorized={false} auth={userAuthorized} />
-          }
-        >
-          <Route
-            path="/login"
-            element={<Login authorizedFunc={setUserAuthorized} />}
-          />
-        </Route>
       </Routes>
-      {!userAuthorized && <RouteMenu />}
+      {userAuthorized && location.pathname !== '/' && <RouteMenu />}
     </div>
-  );
+  )
 }
