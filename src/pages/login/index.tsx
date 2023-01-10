391eb130e159ec3d9e944878fccd59f29ad457e5
Daniil <kashel369@bk.ru>
2023-01-10 19:46:38 +0300

    first api file checking

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,51 +1,12 @@
 import style from './login.module.css'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { UserInformation } from '../../components/user-information/user-information'
-import { TFormValues, TUserPhoto, TUserSubmitValues, TUserWithoutPhoto } from '../../utils/types'
-import { checkResponse } from '../../utils/api'
+import { TFormValues } from '../../utils/types'
+import { postUser } from '../../utils/api'
 
 export default function Login({ authorizedFunc }: { authorizedFunc: Function }) {
   const [photoChanged, setPhotoChanged] = useState(false)
   const onPhotoChange = () => setPhotoChanged(true)
-  const mainUrl = 'http://95-163-235-246.cloudvps.regruhosting.ru:8080'
-
-  const postUser = (content: TUserWithoutPhoto) => {
-    // eslint-disable-next-line
-    const url = mainUrl + '/users' + '/create'
-    return fetch(url, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      method: 'POST',
-      body: JSON.stringify(content),
-    }).then(checkResponse)
-  }
-
-  const postUserPhoto = (content: TUserPhoto, id: number) => {
-    // eslint-disable-next-line
-    const url = mainUrl + '/users' + id + '/photo'
-    return fetch(url, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      method: 'POST',
-      body: JSON.stringify(content),
-    }).then(checkResponse)
-  }
-
-  // const getTest = () => {
-  //   const url = mainUrl + '/users'
-  //   return fetch(url, {
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     method: 'GET',
-  //   }).then(checkResponse)
-  // }
-
-  // useEffect(() => {
-  //   getTest().then((data) => console.log(data))
-  // }, [])
 
   const onSubmit = (data: TFormValues) => {
     const { name, about, work } = data
--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -1,1 +1,88 @@
+/* eslint-disable no-useless-concat */
+import { TUserPhoto, TUserWithoutPhoto } from './types'
+
+const mainUrl = 'http://95-163-235-246.cloudvps.regruhosting.ru:8080'
+
 export const checkResponse = (res: any) => (res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`))
+
+export const postUser = (content: TUserWithoutPhoto) => {
+  const url = mainUrl + '/users' + '/create'
+  return fetch(url, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+    body: JSON.stringify(content),
+  }).then(checkResponse)
+}
+
+// ----------
+const createFormData = (content: TUserPhoto) => {
+  const data = new FormData()
+  data.set('file', content.file)
+  return data
+}
+
+export const postUserPhoto = (content: TUserPhoto, id: number) => {
+  const url = mainUrl + '/users/' + id + '/photo'
+  return fetch(url, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+    method: 'POST',
+    body: createFormData(content),
+  }).then(checkResponse)
+}
+// ----------
+
+export const getAllUsers = () => {
+  const url = mainUrl + '/users'
+  return fetch(url, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method: 'GET',
+  }).then(checkResponse)
+}
+
+export const getUserById = (userId: number) => {
+  const url = mainUrl + '/users/' + userId
+  return fetch(url, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method: 'GET',
+  }).then(checkResponse)
+}
+
+export const getUserFavorites = (ownerId: number) => {
+  const url = mainUrl + '/likes/' + ownerId
+  return fetch(url, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method: 'GET',
+  }).then(checkResponse)
+}
+
+export const addUserLike = (ownerId: number, userId: number) => {
+  const url = mainUrl + '/likes' + '/create'
+  return fetch(url, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+    body: JSON.stringify({ ownerId, userId }),
+  })
+}
+
+export const removeUserLike = (ownerId: number, userId: number) => {
+  const url = mainUrl + '/likes' + '/create'
+  return fetch(url, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    method: 'POST',
+    body: JSON.stringify({ ownerId, userId }),
+  })
+}
