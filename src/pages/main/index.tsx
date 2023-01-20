7fb4858e581e48bd70e34c98ac91ccc7d10fc301
Aliaksei Levashenia <mrfreeze987@gmail.com>
2023-01-20 12:36:02 +0300

    login fix, api fix, users link fix

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,21 +1,10 @@
 import CardWrapper from '../../components/card-wrapper/card-wrapper';
 import { useEffect, useState } from 'react';
-
+import { getAllUsers } from '../../utils/api';
 export default function Main() {
   const [cards, setInitialCards] = useState([]);
-  const getInitialCards = () => {
-    return fetch(`http://95-163-235-246.cloudvps.regruhosting.ru:8080/users`, {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json' },
-    }).then((res) => {
-      if (res.ok) {
-        return res.json();
-      }
-      return Promise.reject(`Ошибка: ${res.status}`);
-    });
-  };
   useEffect(() => {
-    getInitialCards()
+    getAllUsers(localStorage.getItem('ownerId'))
       .then((card) => {
         setInitialCards(card);
         console.log(card);
--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -1,38 +1,40 @@
 /* eslint-disable no-useless-concat */
 
-const mainUrl = 'http://weetalk.online/api/v1'
+const mainUrl = 'http://weetalk.online/api/v1';
 
-export const checkResponse = (res: any) => (res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`))
+export const checkResponse = (res: any) =>
+  res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
 
-export const checkResponseWithoutContent = (res: any) => !res.ok && Promise.reject(`Ошибка: ${res.status}`)
+export const checkResponseWithoutContent = (res: any) =>
+  !res.ok && Promise.reject(`Ошибка: ${res.status}`);
 
 export const postUser = (content: FormData) => {
-  const url = mainUrl + '/users'
+  const url = mainUrl + '/users';
   return fetch(url, {
     method: 'POST',
     body: content,
-  }).then(checkResponse)
-}
+  }).then(checkResponse);
+};
 
-export const getAllUsers = () => {
-  const url = mainUrl + '/users'
+export const getAllUsers = (userId: string | null) => {
+  const url = mainUrl + `/users?id=${userId}`;
   return fetch(url, {
     headers: {
       'Content-Type': 'application/json',
     },
     method: 'GET',
-  }).then(checkResponse)
-}
+  }).then(checkResponse);
+};
 
 export const getUserById = (userId: number | string) => {
-  const url = mainUrl + '/users/' + userId
+  const url = mainUrl + '/users/' + userId;
   return fetch(url, {
     headers: {
       'Content-Type': 'application/json',
     },
     method: 'GET',
-  }).then(checkResponse)
-}
+  }).then(checkResponse);
+};
 
 // !Не готово
 // export const patchUserById = (userId: number) => {
