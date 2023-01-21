6b2fda46a44149fc1999ad01cf66039b23e61717
Daniil <kashel369@bk.ru>
2023-01-22 01:24:26 +0300

    favorite tip logic change

--- src/pages/favorites/index.tsx
+++ src/pages/favorites/index.tsx
@@ -1,52 +1,41 @@
-import React, { useEffect, useState } from 'react';
-import CardWrapper from '../../components/card-wrapper/card-wrapper';
-import style from '../../components/card-wrapper/card-wrapper.module.css';
-import Card from '../../components/card/card';
-import { TipModal } from '../../components/tip-modal/tip-modal';
-import { TTipPopupOffset } from '../../utils/types';
-import favoritesStyle from './favorites.module.css';
-import { imageVariable } from '../../utils/imageVariable';
-import { TitleSmart } from '../../components/title-smart/title-smart';
-import { getUserFavorites } from '../../utils/api';
+import React, { useEffect, useRef, useState } from 'react'
+import CardWrapper from '../../components/card-wrapper/card-wrapper'
+import style from '../../components/card-wrapper/card-wrapper.module.css'
+import Card from '../../components/card/card'
+import { TipModal } from '../../components/tip-modal/tip-modal'
+import { TTipPopupOffset } from '../../utils/types'
+import favoritesStyle from './favorites.module.css'
+import { imageVariable } from '../../utils/imageVariable'
+import { TitleSmart } from '../../components/title-smart/title-smart'
+import { getUserFavorites } from '../../utils/api'
 
 function Favorites() {
-  const [heartCords, setHeartCords] = useState<TTipPopupOffset | null>(null);
-  const [favorites, setFavorites] = useState([]);
-  const [isFavoritesLoaded, setIsFavoritesLoaded] = useState(false);
+  const [heartCords, setHeartCords] = useState<TTipPopupOffset | null>(null)
+  const [favorites, setFavorites] = useState([])
+  const [isFavoritesLoaded, setIsFavoritesLoaded] = useState(false)
+  const cardWrapperRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
-    const getElementByQuery = (str: string) =>
-      new Promise<Element>(resolve => {
-        const getElement = () => {
-          const element = document.querySelector(str);
-          if (element) {
-            resolve(element);
-          } else {
-            requestAnimationFrame(getElement);
-          }
-        };
-        getElement();
-      });
-
-    if (isFavoritesLoaded && favorites.length === 0) {
-      getElementByQuery('.card_heart__zgeMe').then(element => {
-        const { top, left } = element.getBoundingClientRect();
-        setHeartCords({ top, left });
-      });
+    const cardWrapper = cardWrapperRef?.current
+    if (cardWrapper) {
+      const likeButton = cardWrapper.querySelector('button')
+      if (likeButton) {
+        const { top, left } = likeButton.getBoundingClientRect()
+        setHeartCords({ top, left })
+      }
     }
-  }, [isFavoritesLoaded, favorites]);
+  }, [isFavoritesLoaded, favorites, cardWrapperRef])
 
   useEffect(() => {
     getUserFavorites(String(localStorage.getItem('ownerId')))
-      .then(card => {
-        setFavorites(card);
+      .then((card) => {
+        setFavorites(card)
       })
-      .catch(error => console.log(`Error: ${error}`))
-      .finally(() => setIsFavoritesLoaded(true));
-  }, []);
+      .catch((error) => console.log(`Error: ${error}`))
+      .finally(() => setIsFavoritesLoaded(true))
+  }, [])
 
-  const tipMessage =
-    'Иван, ты пока ещё никого не добавил в избранное 😔 \n Это легко сделать, нажав на иконку';
+  const tipMessage = 'Иван, ты пока ещё никого не добавил в избранное 😔 \n Это легко сделать, нажав на иконку'
 
   return isFavoritesLoaded && favorites.length === 0 ? (
     <div className={style.wrapper}>
@@ -58,33 +47,22 @@ function Favorites() {
         wrapperStyle={style.heading}
       />
       <div style={{ height: '100px' }}></div>
-      <Card
-        username="Евгений Александров"
-        actualJob="Дизайнер в Gradient"
-        image={imageVariable}
-        isLiked={false}
-        description="Профессиональный дизайнер, опыт работы 8 лет. Основатель комьюнити “Контраст”. Занимаюсь йогой, люблю отдыхать на природе. Буду рад обменяться опытом построения сообщества!"
-        card={{}}
-        onCardClick={() => {}}
-      />
-      {heartCords && (
-        <TipModal
-          message={tipMessage}
-          offset={heartCords}
-          onClick={() => {}}
-          overlayAdditionStyle={favoritesStyle.TipAdditionStyle}
+      <div className={style.cardWrapper} ref={cardWrapperRef}>
+        <Card
+          username="Евгений Александров"
+          actualJob="Дизайнер в Gradient"
+          image={imageVariable}
+          isLiked={false}
+          description="Профессиональный дизайнер, опыт работы 8 лет. Основатель комьюнити “Контраст”. Занимаюсь йогой, люблю отдыхать на природе. Буду рад обменяться опытом построения сообщества!"
+          card={{}}
+          onCardClick={() => {}}
         />
-      )}
+      </div>
+      {heartCords && <TipModal message={tipMessage} offset={heartCords} onClick={() => {}} overlayAdditionStyle={favoritesStyle.TipAdditionStyle} />}
     </div>
   ) : (
-    <CardWrapper
-      title="Избранное"
-      array={favorites}
-      favorites={favorites}
-      setFavorites={setFavorites}
-      isFavoritesLoaded={isFavoritesLoaded}
-    />
-  );
+    <CardWrapper title="Избранное" array={favorites} favorites={favorites} setFavorites={setFavorites} isFavoritesLoaded={isFavoritesLoaded} />
+  )
 }
 
-export default Favorites;
+export default Favorites
