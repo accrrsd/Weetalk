651964be68efda99e51c72f153ba3e72a3f7f7e3
Aliaksei Levashenia <mrfreeze987@gmail.com>
2023-02-15 22:36:10 +0300

    profile import fix

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -6,7 +6,7 @@ import { ProtectedRoutes } from '../protected-routes/protected-routes'
 
 import Login from '../../pages/login'
 import { RouteMenu } from '../route-menu/route-menu'
-import Profile from '../../pages/profile'
+import Profile from '../../pages/Profile'
 import Guests from '../../pages/guests'
 import Welcome from '../../pages/welcome'
 import Advices from '../../pages/advices'
@@ -30,12 +30,39 @@ export default function App() {
   return (
     <div className={style.page}>
       <Routes>
-        <Route element={<ProtectedRoutes needCondition={false} condition={userAuthorized} redirect="/" />}>
-          <Route path="/login" element={<Login authorizedFunc={setUserAuthorized} />} />
+        <Route
+          element={
+            <ProtectedRoutes
+              needCondition={false}
+              condition={userAuthorized}
+              redirect="/"
+            />
+          }
+        >
+          <Route
+            path="/login"
+            element={<Login authorizedFunc={setUserAuthorized} />}
+          />
         </Route>
 
-        <Route element={<ProtectedRoutes needCondition={true} condition={userAuthorized} redirect="/login" />}>
-          <Route element={<ProtectedRoutes needCondition={false} condition={welcomeDone} redirect="/guests" />}>
+        <Route
+          element={
+            <ProtectedRoutes
+              needCondition={true}
+              condition={userAuthorized}
+              redirect="/login"
+            />
+          }
+        >
+          <Route
+            element={
+              <ProtectedRoutes
+                needCondition={false}
+                condition={welcomeDone}
+                redirect="/guests"
+              />
+            }
+          >
             <Route path="/" element={<Welcome />} />
           </Route>
           <Route path="/guests" element={<Guests />} />
